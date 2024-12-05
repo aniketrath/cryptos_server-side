b55f695bcf49541dab998c859c36c07ff341f2c9
aniketrath <aniketrath1121@gmail.com>
2024-12-05 16:02:01 +0000

    Added Prometheus and Grafana.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const client = require('prom-client'); // Import prom-client for Prometheus
 // Import the database connection function
 const connectDB = require('./config/database');
-//Import Routes :
+// Import Routes :
 const updateDatabase = require('./app/routes/updateRoutes');
 const databaseRoutes = require('./app/routes/databaseRoutes');
 const authRoutes = require('./app/routes/authRoutes')
@@ -11,23 +12,45 @@ const authenticateJWT = require('./middlewares/authMiddleware');
 // Initialize Express app @6000 by env
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Create a Registry and define Metrics
+const register = new client.Registry();
+const httpRequestsTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total HTTP requests',
+  labelNames: ['method', 'route', 'status']
+});
+register.registerMetric(httpRequestsTotal);
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 connectDB();
 
+// Log and count requests for Prometheus
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    // Increment the counter for each request, using method, route, and status
+    httpRequestsTotal.inc({ method: req.method, route: req.route?.path || 'unknown', status: res.statusCode });
+  });
+  next();
+});
 
 // Calling Imported Routes with Prefixes :
 // Use the authentication routes
 app.use('/auth', authRoutes);
-app.use('/update',authenticateJWT, updateDatabase);
-app.use('/app',databaseRoutes);
-
-
+app.use('/update', authenticateJWT, updateDatabase);
+app.use('/app', databaseRoutes);
 
 // Default route to check if the server is working
 app.get('/', (req, res) => {
-    console.log("Server is Accessed")
-    res.send('Server is running');
+  console.log("Server is Accessed");
+  res.send('Server is running');
+});
+
+// Expose the /metrics endpoint for Prometheus to scrape
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
 
 // Start the server
